4f4c417c5382353e171bf692528cfe1595e19a28
lws <chenkun_lws@126.com>
2019-05-12 00:02:08 +0800

    simple test example
    Signed-off-by: lws <chenkun_lws@126.com>

--- src/main.rs
+++ src/main.rs
@@ -33,3 +33,13 @@ fn panic(panic_info: &PanicInfo) -> ! {
 fn panic(panic_info: &PanicInfo) -> ! {
     flandre_os::test_panic_handler(panic_info)
 }
+
+#[test_case]
+fn trivial_assertion() {
+    use flandre_os::serial_print;
+    use flandre_os::serial_println;
+
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
