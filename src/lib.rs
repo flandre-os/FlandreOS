3c074520c2b3c7661db55ac9a057c1c65cd79ae2
lws <chenkun_lws@126.com>
2019-05-11 23:27:58 +0800

    set up test environment
    Signed-off-by: lws <chenkun_lws@126.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,50 @@
 #![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use core::panic::PanicInfo;
+
+use crate::qemu::{exit_qemu, QemuExitCode};
+#[cfg(test)]
+use bootloader::entry_point;
+#[cfg(test)]
+use bootloader::BootInfo;
+
+pub mod qemu;
 pub mod serial;
 pub mod vga_buffer;
 
-#[macro_export]
-macro_rules! print {
-    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
 }
 
-#[macro_export]
-macro_rules! println {
-    () => ($crate::print!("\n"));
-    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[Failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    hlt_loop()
+}
+
+#[cfg(test)]
+entry_point!(entry);
+
+#[cfg(test)]
+fn entry(_boot_info: &'static BootInfo) -> ! {
+    test_main();
+
+    hlt_loop()
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
 }
 
 pub fn hlt_loop() -> ! {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(flandre_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use bootloader::entry_point;
 use bootloader::BootInfo;
@@ -10,14 +13,23 @@ use flandre_os::println;
 entry_point!(entry);
 
 fn entry(_boot_info: &'static BootInfo) -> ! {
-    let user_name = "lws";
-    println!("Hello, {}!", user_name);
+    println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
 
     hlt_loop()
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(panic_info: &PanicInfo) -> ! {
     println!("{}", panic_info);
     hlt_loop()
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(panic_info: &PanicInfo) -> ! {
+    flandre_os::test_panic_handler(panic_info)
 }
