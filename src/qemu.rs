ae0bf2379175f1a64e4ef892fbddfdcc0234f3d2
lws <chenkun_lws@126.com>
2019-05-11 23:58:19 +0800

    more document about the const value
    Signed-off-by: lws <chenkun_lws@126.com>

--- src/qemu.rs
+++ src/qemu.rs
@@ -1,3 +1,6 @@
+/// QEMU exit with exit status (value << 1) | 1
+/// (0x10 << 1) | 1 = 0x101 = 33
+/// (0x11 << 1) | 1 = 0x111 = 35
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
@@ -9,6 +12,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     use x86_64::instructions::port::Port;
 
     unsafe {
+        // 0xf4 is the iobase of the isa-debug-exit device
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
