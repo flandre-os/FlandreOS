291c8741f4794c86c277f84152b3aac758c7fd01
lws <chenkun_lws@126.com>
2019-05-13 01:43:40 +0800

    format code
    Signed-off-by: lws <chenkun_lws@126.com>

--- src/vga_buffer/buffer.rs
+++ src/vga_buffer/buffer.rs
@@ -2,15 +2,15 @@ use volatile::Volatile;
 
 use crate::vga_buffer::color::ColorCode;
 
+pub const BUFFER_HEIGHT: usize = 25;
+pub const BUFFER_WIDTH: usize = 80;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 pub struct ScreenChar {
     pub ascii_character: u8,
     pub color_code: ColorCode,
 }
-
-pub const BUFFER_HEIGHT: usize = 25;
-pub const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 pub struct Buffer {
